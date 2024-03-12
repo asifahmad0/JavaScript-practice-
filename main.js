214bd07f698393d5f46e3f4c8c19f8cbe293010d
ASIF AHMAD <asifahma7761@gmail.com>
2024-03-12 22:30:03 +0530

    javascript

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-alert("ths is alert");
+/*var alart="ths is alert"
+
+alert(alart);
 document.write('this is document');
 prompt("enter your feedback","good function");
 confirm("do you want to conform");
+*/
+
+var x =document.getElementsByClassName("main").innerHTML;
+console.log(x);
+
