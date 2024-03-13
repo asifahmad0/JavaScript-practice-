88fb2f56168ebe7f44daac030e3c7d404d345c77
asif <asifahma771@gmail.com>
2024-03-13 15:03:29 +0530

    s

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.write('this is document');
 prompt("enter your feedback","good function");
 confirm("do you want to conform");
 */
-
-var x =document.getElementsByClassName("main").innerHTML;
-console.log(x);
-
+//var x =document.getElementsByClassName("main")[0].innerHTML;
+//console.log(x);
+//get= prompt("entre some thing", "entre here ")
+//var id =document.getElementById("myid").innerHTML=get;
