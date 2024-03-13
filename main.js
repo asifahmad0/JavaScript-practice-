4aa9ab8be44f0fc5ff35885028e9f59d2e06d5b6
ASIF AHMAD <asifahma7761@gmail.com>
2024-03-13 22:49:22 +0530

    some new

--- main.js
+++ main.js
@@ -5,7 +5,20 @@ document.write('this is document');
 prompt("enter your feedback","good function");
 confirm("do you want to conform");
 */
+
 //var x =document.getElementsByClassName("main")[0].innerHTML;
 //console.log(x);
 //get= prompt("entre some thing", "entre here ")
 //var id =document.getElementById("myid").innerHTML=get;
+
+
+/*var x =document.getElementsByClassName("main").innerHTML;
+console.log(x);*/
+
+/*
+myid.innerHTML="my name is asif"; //myid is aid of html div
+document.getElementById("myid").style.fontSize="2em"; //use css like this mathord 
+document.getElementById("myid").style.background="blue"; //use css like this mathord 
+document.getElementById("myid").style.color="white"; //use css like this mathord
+*/
+
