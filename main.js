69ad29a3fbdd88cd70e14b10ed27618b7bb3c9fd
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2024-03-24 12:48:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,4 +21,136 @@ document.getElementById("myid").style.fontSize="2em"; //use css like this mathor
 document.getElementById("myid").style.background="blue"; //use css like this mathord 
 document.getElementById("myid").style.color="white"; //use css like this mathord
 */
+//console.error("this is error")
+//console.warn("this is a warning")
+//--------------------------------------------------variables--------------------------------------
+/*
+var str="my name is asif "; // string variable
+let age= 22; // integre variable
+const buln= true; // boolen variable
+const student= { // object variable 
+    name:"anam",
+    roll: 20, 
+    cgpa:8.5,
+    age:22
+}
+student.name = "asif ahmad" //changed name 
+student.age = student.age +1 // increas age by one 
+console.log(student.name); 
+console.log(student["age"]);
+console.log(typeof(student));
+*/
+//===========================================Arathmatics Oprator======================================
 
+// let a= 5;
+// let b= 2;
+
+// console.log("5 + 2 =", a+b);
+// console.log("5 - 2 =", a-b);
+// console.log("5 * 2 =", a*b);
+// console.log("5 / 2 =", a/b);
+// console.log("5 % 2 =", a%b);
+// console.log("5^2 =", a**b);
+
+//========================================for loop===============================================
+
+// sum of n numbres and print 
+/*
+let num =prompt("entre your numbre ")
+let sum=0;
+for(i=1; i<=num; i++) {
+    sum+=i
+}
+document.write(sum)
+
+for(i=1; i<=5; i++){
+     console.log('i=',i)
+}
+
+//Odd numbre print
+for(a=1; a<="10"; a++){
+    if(a%2 !==0){
+    console.log(a)}
+}
+
+// print table
+let a= prompt("entre your numbre ")
+
+for(b=1; b<10; b++){
+console.log(a,"*",b,"= ",a*b,"\n");
+}
+
+
+*/
+
+//--------------------------------------------for of loop use for str and arrys
+/*// let str= "my name is asif ahmad";
+// for(i of str){
+//     console.log(i)
+// }
+var arr = [25,55,68,62,75,92,48];
+for(y of arr){
+    var x= prompt("Gesh the Numbre what I think"," entre your Answer ");
+    if(x==y){
+        alert("congrats your Gesh is currect, your are genias  ");
+    }else{
+        alert("You are wrong, currect numbre is  ",y);
+    }
+}
+*/
+
+
+//---------------------------------------------------for in loop use for object
+// let student= { // object variable 
+//     name:"anam",
+//     roll: 20, 
+//     cgpa:8.5,
+//     age:22
+// };
+
+// for(i in student){
+//     console.log(i)
+// }
+//=========================================while loop
+// let i=1;
+// while( i<=5){
+//     console.log(i)
+//     i+=1
+// }
+//-------------------------------------do while loop run only one time 
+
+// let i=20;
+// do{
+//     console.log("my name is asif");
+// }while(i<=5){
+//     console.log("my name is asif ");
+// }
+
+//-----------------------------------------------STRINGS-----------------------------------------------
+
+//let x = " My Name is Asif ";// str1
+//let a = " roll no 26"; //str 2
+
+//var y= x.length;
+//var z =`my name is asif ahmad${5*5} `; //spacile string we can proform multyple opration in a string
+//var y= x.toUpperCase()// for upper case
+//var y= x.toLowerCase()// for lower case
+//var y=x.trim() // eres white space from start to end 
+//var y = x.slice(12var,16) // get the str from idx 12 to idx 15 
+// y = a.concat(x); // add 2 str i one line 
+//var y = x.replace ("Asif", "Anam"); //replace methord use .replace()/ .replaceAll() 
+//var y = x.charAt(12) // finde the charectre from given index
+//console.log(y);
+
+// get value from user and creat a user name for user 
+/*
+ var a = prompt("entr your name ");
+ var mobile = prompt("entr your mobile ");
+ var b = "@";
+ let c = mobile.charAt(9);
+ let y = b+a+c;
+console.log(y);
+*/
+
+
+//------------------------------------------------------
