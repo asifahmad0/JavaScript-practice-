7fe8c321198babad5f0c45f2c2d8b3e5fe79861f
ASIF AHMAD <67062945+asifahmad0@users.noreply.github.com>
2024-04-14 11:09:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -330,6 +330,7 @@ alert(a);*/
 
 // let head= document.querySelector('h2');
 // head.setAttribute('class', 'css');
+// head.classList.add('newClass');
 // let div=document.querySelector('#myid');
 // let btn=document.createElement('button');
 // div.append(btn);
